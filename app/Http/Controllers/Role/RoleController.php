7b3f069c710da5af63bb8b88e566b50378934b60
domicel <domicita098@gmail.com>
2026-08-19 16:43:07 -0400

    creado index para roles.

---
+++ app/Http/Controllers/Role/RoleController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\Role;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $sort = $request->input('sort', 'name') ?: 'name';
+        $order = $request->input('order', 'asc') ?: 'asc';
+        $limit = $request->input('limit', 10);
+        $search = $request->input('q');
+
+        $rolesQuery = Role::query()
+            ->select(['id', 'name'])
+            ->when($search, function ($query, $search) {
+                $query->where('name', 'like', "%{$search}%");
+            })
+            ->orderBy($sort, $order);
+
+        $roles = $rolesQuery->paginate($limit)->withQueryString();
+
+        return Inertia::render('Roles/Index', [
+            'roles' => $roles,
+            'filters' => $request->only(['q', 'sort', 'order', 'limit']),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
