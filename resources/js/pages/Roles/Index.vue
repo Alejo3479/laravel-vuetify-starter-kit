5b6fea260076856249f1994d2c9a83d3d17a5e30
Gustavo Andrés Gutiérrez Gaspar <gustavog095065@gmail.com>
2026-08-20 12:07:37 -0400

    closed #13 Vista del listado vacio

---
+++ resources/js/pages/Roles/Index.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+import { index as rolesIndex } from '@/routes/roles';
+
+
+
+defineOptions({
+    layout: {
+        breadcrumbs: [
+            {
+                title: 'Roles',
+                href: rolesIndex(),
+            },
+        ],
+    },
+});
+
+
+</script>
+
+<template>
+    <Head title="Roles" />
+
+    <div class="app-page">
+        <VCard>
+            <VCardTitle>Listado de Roles</VCardTitle>
+            <VDivider />
+            <VCardText>
+                <VDataTable
+
+                />
+            </VCardText>
+        </VCard>
+    </div>
+</template>
