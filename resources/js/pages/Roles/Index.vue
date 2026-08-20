7b602eba20e7b460b0b2e14db4e700f6a26e02eb
Gustavo Andrés Gutiérrez Gaspar <gustavog095065@gmail.com>
2026-08-20 15:08:55 -0400

    fixed_3 #13 Correccion de error por offset sin limit. Vuetify manda -1

--- resources/js/pages/Roles/Index.vue
+++ resources/js/pages/Roles/Index.vue
@@ -73,7 +73,14 @@ const onUpdateOptions = ({
                     :headers="headers"
                     :items="roles.data"
                     :items-length="roles.total"
-
+                    :items-per-page="roles.per_page"
+                    :items-per-page-options="[
+                        { value: 10, title: '10' },
+                        { value: 25, title: '25' },
+                        { value: 50, title: '50' },
+                    ]"
+                    :page="roles.current_page"
+                    item-value="id"
                     @update:options="onUpdateOptions"
                 />
             </VCardText>
