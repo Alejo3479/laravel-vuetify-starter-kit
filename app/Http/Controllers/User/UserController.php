f2dc55ac9a84604dad8d880e6326fe1a6e8d42f2
domicel <domicita098@gmail.com>
2026-08-20 12:18:11 -0400

    creado funcion index en el controlador de usuarios . closed #14 .

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $sort = $request->input('sort', 'name') ?: 'name';
+        $order = $request->input('order', 'asc') ?: 'asc';
+        $limit = $request->input('limit', 10);
+        $search = $request->input('q');
+        $usersQuery = User::query()
+            ->select(['id', 'name', 'email'])
+            ->when($search, function ($query, $search) {
+                $query->where('name', 'like', "%{$search}%");
+            })
+            ->orderBy($sort, $order);
+        $users = $usersQuery->paginate($limit)->withQueryString();
+        return Inertia::render('Users/Index', [
+            'users' => $users,
+            'filters' => $request->only(['q', 'sort', 'order', 'limit']),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
