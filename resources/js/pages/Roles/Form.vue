952b35a09541b202eada1e6ec892132376b865e0
Gustavo Andrés Gutiérrez Gaspar <gustavog095065@gmail.com>
2026-08-21 09:45:53 -0400

    closed #22 Creacion de la vista de edicion o Nuevo rol

---
+++ resources/js/pages/Roles/Form.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { index as rolesIndex } from '@/routes/roles';
+
+defineOptions({
+    layout: {
+        breadcrumbs: [
+            {
+                title: 'Roles',
+                href: rolesIndex(),
+            },
+        ],
+    },
+});
+</script>
+
+<template>
+    <div class="app-page">
+        <VCard>
+            <VCardTitle>Edición/Nuevo</VCardTitle>
+            <VDivider />
+            <VCardText>
+                <VTextField
+                    label="Nombre"
+                    variant="outlined"
+                    density="compact"
+                />
+                <div class="mt-4">
+                    <p class="text-body-2 mb-2">Permisos</p>
+                    <VCheckbox
+                    />
+                </div>
+                <VBtn class="mt-4" color="primary">Guardar</VBtn>
+            </VCardText>
+        </VCard>
+    </div>
+</template>
