7ff81b6d4aa977339a1106e9bdf630e03c73856c
domicel <domicita098@gmail.com>
2026-08-27 17:44:59 -0400

    permisos en rutas de clientes . closed #64 .

--- routes/clientes.php
+++ routes/clientes.php
@@ -4,6 +4,16 @@ use App\Http\Controllers\Cliente\ClienteController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('clientes', ClienteController::class);
+    Route::get('/clientes', [ClienteController::class, 'index'])->name('clientes.index')->middleware('can:listar.clientes');
+    
+    Route::get('/clientes/create', [ClienteController::class, 'create'])->name('clientes.create')->middleware('can:crear.clientes');
+    Route::post('/clientes', [ClienteController::class, 'store'])->name('clientes.store')->middleware('can:crear.clientes');
+    
+    Route::get('/clientes/{cliente}', [ClienteController::class, 'show'])->name('clientes.show')->middleware('can:ver.clientes');
+    
+    Route::get('/clientes/{cliente}/edit', [ClienteController::class, 'edit'])->name('clientes.edit')->middleware('can:editar.clientes');
+    Route::put('/clientes/{cliente}', [ClienteController::class, 'update'])->name('clientes.update')->middleware('can:editar.clientes');
+    
+    Route::delete('/clientes/{cliente}', [ClienteController::class, 'destroy'])->name('clientes.destroy')->middleware('can:eliminar.clientes');
         
 });
